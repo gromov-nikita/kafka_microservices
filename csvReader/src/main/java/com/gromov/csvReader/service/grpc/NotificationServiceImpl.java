5b40ac146cc52d35d7d69ffe12881f2e0e0c1722
gromov-nikita <hromay.n999@gmail.com>
2025-06-15 12:22:28 +0300

    "let's use logging here"

--- csvReader/src/main/java/com/gromov/csvReader/service/grpc/NotificationServiceImpl.java
+++ csvReader/src/main/java/com/gromov/csvReader/service/grpc/NotificationServiceImpl.java
@@ -4,13 +4,19 @@ import com.gromov.notification_grpc.EventRequest;
 import com.gromov.notification_grpc.EventResponse;
 import com.gromov.notification_grpc.NotificationServiceGrpc;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @GrpcService
+@Slf4j
 public class NotificationServiceImpl extends NotificationServiceGrpc.NotificationServiceImplBase {
+
+
     @Override
     public void notifyEvent(EventRequest request, StreamObserver<EventResponse> responseObserver) {
-        System.out.println("Received event: " + request.getEventId() + ", message: " + request.getMessage());
+        log.warn("Received event: eventId={}, message={}",request.getEventId(),request.getMessage());
         EventResponse response = EventResponse.newBuilder()
                 .setSuccess(true)
                 .setDetails("Event processed successfully")
