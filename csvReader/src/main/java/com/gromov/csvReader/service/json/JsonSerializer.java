fcb71d8aa32a39f1e97eace9c780464d4338a706
gromov-nikita <hromay.n999@gmail.com>
2025-06-16 09:06:19 +0300

    "It's more like Component"

--- csvReader/src/main/java/com/gromov/csvReader/service/json/JsonSerializer.java
+++ csvReader/src/main/java/com/gromov/csvReader/service/json/JsonSerializer.java
@@ -2,9 +2,10 @@ package com.gromov.csvReader.service.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+@Component
 public class JsonSerializer {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
